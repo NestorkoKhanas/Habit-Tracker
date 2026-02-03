f972937f754fc7ed1ccb4ad2228a52cd6c48912f
Nestor <52535323+NestorkoKhanas@users.noreply.github.com>
2026-02-03 15:25:48 +0200

    Convert mainScreen to stateFulWidget

--- lib/features/home/main_screen.dart
+++ lib/features/home/main_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/core/theme/colors.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(

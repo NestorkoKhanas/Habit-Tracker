7c94233d99218e4800d55e7822cda8c4940eabdc
Nestor <52535323+NestorkoKhanas@users.noreply.github.com>
2026-02-03 14:04:49 +0200

    Add main app colors

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color deepestIndigo = Color(0xFF2E365A);
+const Color mutedPurple = Color(0xFF6C5E82);
+const Color dustyLavender = Color(0xFFAC91A7);
+const Color softCoral = Color(0xFFD07B7F);
+const Color warmPink = Color(0xFFE07B7F);
+const Color lightBlueGrey = Color(0xFF96A4C5);
+const Color midBlue = Color(0xFF3F5B8B);
+const Color lightTextColor = Color.fromARGB(200, 255, 255, 255);
+const Color darkTextColor = deepestIndigo;

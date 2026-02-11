1ad3db4e9a9d47d03337c6e6241c084351516690
Nestor <52535323+NestorkoKhanas@users.noreply.github.com>
2026-02-11 23:44:32 +0200

    Add List builder for habit

--- lib/features/habits/ui/screens/habit_screen.dart
+++ lib/features/habits/ui/screens/habit_screen.dart
@@ -27,7 +27,64 @@ class HabitScreen extends StatelessWidget {
         ),
       ),
       body: Column(
-        children: [const Center(child: Text('Welcome to Habit Tracker!'))],
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.only(top: 8, left: 8),
+            child: Text(
+              "Select Date:",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Container(
+            height: 100,
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 50,
+              itemBuilder: (context, index) => Container(
+                width: 70,
+                decoration: BoxDecoration(
+                  color: Colors.blue[200],
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+
+                child: Center(child: Text('Day: ${index + 1}')),
+              ),
+            ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: 300,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Add Habit',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8), // spacing between buttons and list
+          Expanded(
+            child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) => Container(
+                decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: ListTile(title: Text('Habit ${index + 1}')),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
